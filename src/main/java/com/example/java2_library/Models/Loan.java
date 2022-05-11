5d59be9c688d8356e99a8d5a423fdee4692888bb
hanan <hanrai-0@student.ltu.se>
2022-05-11 11:31:42 +0200

    h

--- src/main/java/com/example/java2_library/Models/Loan.java
+++ src/main/java/com/example/java2_library/Models/Loan.java
@@ -1,16 +1,28 @@
 package com.example.java2_library.Models;
 
+import java.util.Date;
+
 public class Loan {
     private int loanNo;
     private int userId; // behöver synkas med user
     private int barcode; // behöver synkas med copy
     private int startDate;
+    private Date dateReturned;
+    private Book book;
 
-    public Loan(int loanNo, int userId, int barcode, int startDate) {
+    private Receipt receipt;
+
+    private Boolean bookStatus;
+
+    public Loan(int loanNo,Book b, int userId, int barcode, int startDate, Date rDate, Receipt rec, boolean bStatus) {
         this.loanNo = loanNo;
         this.userId = userId;
         this.barcode = barcode;
         this.startDate = startDate;
+        dateReturned = rDate;
+        receipt = rec;
+        bookStatus = bStatus;
+        book = b;
 
     }
 
@@ -45,4 +57,28 @@ public class Loan {
     public void setStartDate(int startDate) {
         this.startDate = startDate;
     }
+    public Boolean getBookStatus() {
+        return bookStatus;
+    }
+
+    public void setDateReturned(Date dateReturned) {
+        this.dateReturned = dateReturned;
+    }
+    public Book getBook()
+    {
+        return book;
+    }
+
+    public void setBookStatus(Boolean bookStatus) {
+        this.bookStatus = bookStatus;
+    }
+    public void BookStatus() {
+        Boolean available = null;
+        if (bookStatus = available)
+        {
+            System.out.println(  getBook().getBookTitle() + "Status: available " );
+                else {
+            System.out.println(  getBook().getBookTitle() + " Status : not available");
+        }
 }
+
